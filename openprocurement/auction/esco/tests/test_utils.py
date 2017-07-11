6f201f214ca69131981158e8a1812e4fba1c784b
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-07-11 14:20:25 +0300

    Update npv calculation

--- openprocurement/auction/esco/tests/test_utils.py
+++ openprocurement/auction/esco/tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-
+from fractions import Fraction
 from openprocurement.auction.esco.utils import calculate_npv
 
 
@@ -8,12 +8,10 @@ from openprocurement.auction.esco.utils import calculate_npv
     ((0.22, 300.600, 270.540, 6), 483.978),  ((0.22, 300.600, 270.540, 6, 125), 460.946),
     ((0.22, 225.450, 202.905, 4), 499.595),
     ((0.22, 75.150, 67.635, 2), 234.309),
-    ((0.22, 751.500, 676.350, 16), False),
 ], ids=['bid1', 'bid1(incomplete period)',
         'bid2', 'bid2(incomplete period)',
         'bid3',
         'bid4',
-        'too long contractDuration'
         ])
 def test_npv_calculation(test_input, expected):
-    assert calculate_npv(*test_input) == expected
+    assert round(float(calculate_npv(*test_input)), 3) == expected
