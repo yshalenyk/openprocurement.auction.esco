7bcc6830a8bd14505837a1dff6d2c8d1b442e82c
Yaroslav Neznaradko <yarsanich@quintagroup.org>
2017-07-14 18:53:24 +0300

    NBUdiscountRate to root of auction doc

--- openprocurement/auction/esco/auctions/simple.py
+++ openprocurement/auction/esco/auctions/simple.py
@@ -1,6 +1,10 @@
 from openprocurement.auction.worker.auctions import simple
+from openprocurement.auction.worker.utils import prepare_service_stage
 
 FORMATTER = lambda **kw: "{}".format(kw.get('tender_id'))
+MULTILINGUAL_FIELDS = ["title", "description"]
+ADDITIONAL_LANGUAGES = ["ru", "en"]
+ROUNDS = 3
 
 # Indentical methods
 get_auction_info = simple.get_auction_info
@@ -11,8 +15,42 @@ announce_results_data = simple.announce_results_data
 # auction_document['items'] ??
 # auction_document['minimalStep'] ??
 # TODO:
-prepare_auction_document = simple.prepare_auction_document
+def prepare_auction_document(self):
+    self.auction_document.update(
+        {"_id": self.auction_doc_id,
+         "stages": [],
+         "tenderID": self._auction_data["data"].get("tenderID", ""),
+         "procurementMethodType": self._auction_data["data"].get("procurementMethodType", "default"),
+         "TENDERS_API_VERSION": self.worker_defaults["TENDERS_API_VERSION"],
+         "initial_bids": [],
+         "current_stage": -1,
+         "NBUdiscountRate": self._auction_data["data"].get("NBUdiscountRate"),
+         "results": [],
+         "minimalStep": self._auction_data["data"].get("minimalStep", {}),
+         "procuringEntity": self._auction_data["data"].get("procuringEntity", {}),
+         "items": self._auction_data["data"].get("items", []),
+         "value": self._auction_data["data"].get("value", {})}
+    )
+    if self.features:
+        self.auction_document["auction_type"] = "meat"
+    else:
+        self.auction_document["auction_type"] = "default"
 
+    for key in MULTILINGUAL_FIELDS:
+        for lang in ADDITIONAL_LANGUAGES:
+            lang_key = "{}_{}".format(key, lang)
+            if lang_key in self._auction_data["data"]:
+                self.auction_document[lang_key] = self._auction_data["data"][lang_key]
+        self.auction_document[key] = self._auction_data["data"].get(key, "")
+
+    self.auction_document['stages'].append(
+        prepare_service_stage(
+            start=self.startDate.isoformat(),
+            type="pause"
+        )
+    )
+
+    return self.auction_document
 
 # bid["value"] ??
 post_results_data = simple.post_results_data
